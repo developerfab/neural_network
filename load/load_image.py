e6bffe4ee631644f4c10a7d99c10034d896f046e
Cristian Fabricio Rodriguez Romero <fabricio.rodriguez@youse.com.co>
2019-04-10 14:20:21 -0500

    load images and load labels

--- load/load_image.py
+++ load/load_image.py
@@ -1,7 +1,45 @@
 #!/usr/bin/env python3
 
-from loader import Loader
+class LoadImage:
+    """
+    Constructor class
 
-class LoadImage(Loader):
-    def __init__(self):
-        super
+    Params:
+
+    * file_url: Url where is located the file
+    """
+    def __init__(self, file_url):
+        # Open de file
+        my_file = open(file_url, "rb")
+        # Read the firsts 4 bytes(32 bits)
+        self.header = my_file.read(4)
+        # Read the next 4 bytes and convent in integer
+        self.number_of_images = int.from_bytes(my_file.read(4), "big")
+        # Read the next 4 bytes and convent in integer
+        self.number_rows = int.from_bytes(my_file.read(4), "big")
+        # Read the next 4 bytes and convent in integer
+        self.number_columns = int.from_bytes(my_file.read(4), "big")
+        # Empty array where going to save the images 
+        # Each 784 bytes is an image. Is important that 
+        # this is in order.
+        self.images = []
+        dimension = self.number_rows * self.number_columns
+        try:
+            for i in range(0, self.number_of_images):
+                image = list(my_file.read(dimension))
+                self.images.append(image)
+        finally:
+            my_file.close()
+        my_file.close()
+
+l = LoadImage('data/t10k-images-idx3-ubyte')
+print(l.header)
+print(l.number_of_images)
+print(l.number_rows)
+print(l.number_columns)
+# print(l.images)
+print("--------------------")
+for image in l.images:
+    for row in (0, 28):
+        print(image[:28])
+        del image[:28]
--- load/load_label.py
+++ load/load_label.py
@@ -1,21 +1,40 @@
 # Class to read the labels files
 
 class LoadLabel:
+    """
+    Constructor class
+
+    Params:
+
+    * file_url: Url where is located the file
+    """
     def __init__(self, file_url):
+        # Open de file
         my_file = open(file_url, "rb")
+        # Read the firsts 4 bytes(32 bits)
         self.header = my_file.read(4)
+        # Read the next 4 bytes and convent in integer
         self.number_of_items = int.from_bytes(my_file.read(4), "big")
+        # Empty array where going to save the labels
+        # Each byte contain a label. Is important that 
+        # this is in order.
         self.labels = []
         try:
-            group = my_file.read(1)
-            while group != b'':
-                print(group)
-                self.labels.append(group)
+            byte_label = my_file.read(1)
+            while byte_label != b'':
+                self.labels.append(byte_label)
                 aux = my_file.read(1)
                 if aux != b'':
-                    group = ord(aux)
+                    byte_label = ord(aux)
                 else:
                     break
         finally:
             my_file.close()
 
+"""
+Example to load a file:
+l = LoadLabel('data/t10k-labels-idx1-ubyte')
+print(l.header)
+print(l.number_of_items)
+print(l.labels)
+"""
