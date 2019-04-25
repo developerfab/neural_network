3fea64c735ee3123614c8687460e62c37a540597
Cristian Fabricio Rodriguez Romero <fabricio.rodriguez@youse.com.co>
2019-04-25 14:21:31 -0500

    create network from configure file and test to marshal, I should use pickle

--- network.py
+++ network.py
@@ -1,35 +1,31 @@
 # Network class, create and control the neural network
 import json
+import marshal
 
-from layer import Layer 
+from layer import Layer
 from load.load_label import LoadLabel
 from load.load_image import LoadImage
 
 class Network:
-    """
-    Constructor method
-
-    Parameters:
-
-    * layers_information: Array to contain the number of neurons by layer. e.g:
-
-        [784, 16, 16, 10]
-          +   +   +   +
-          |   |   |   +-----> layer(4) with 10 neurons
-          |   |   |
-          |   |   +---------> layer(3) with 16 neurons
-          |   |
-          |   +-------------> Layer(2) with 16 neurons
-          |
-          +-----------------> Layer(1) with 784 neurons (WARNING: Input layer)
-
-    """
-    def __init__(self, layers_information):
+    def __init__(self):
         self.configuration = {}
         self.layers = []
+
+    """
+    create_network
+
+    This method create the network with the specify structure sent in 
+    self.configuration.
+
+    Return: -
+    """
+    def create_network(self, url_file):
+        self.__load_configuration(url_file)
+        layers_information = self.configuration['layers_information']
         account_layers = len(layers_information)
         for number_neurons in range(0, account_layers):
             self.layers.append(Layer(layers_information[number_neurons]))
+
 
     """
     related_layers
@@ -66,7 +62,7 @@ class Network:
             layer.active_neurons()
 
         """
-        When the network is ready uncomment it 
+        When the network is ready uncomment it
 
         for image in images:
             print(image)
@@ -87,10 +83,12 @@ class Network:
             print("+++++++++++++++++++++++++++++++++++++")
             layer.print_values()
 
-    """
-    load_configuration
+    # PRIVATE
 
-    Public method
+    """
+    __load_configuration
+
+    Private method
 
     This method load the configuration saved in json file in the folder
     networks/ , the objective of this file is load the basic configuration
@@ -103,11 +101,9 @@ class Network:
 
     Returns: -
     """
-    def load_configuration(self, name_file):
+    def __load_configuration(self, name_file):
         with open('networks/'+name_file, 'r') as f:
             self.configuration = json.load(f)
-
-    # PRIVATE
 
     """
     __load_input_data
@@ -116,8 +112,21 @@ class Network:
 
     This method load the data images calling LoadImage class.
 
-    Parameters: 
-    * url: Route where there are the data
+    Parameters:
+    * url: Route where there are the data, the json must have the key
+    `layers_information` that contain the layers information like it:
+
+    * layers_information: Array to contain the number of neurons by layer. e.g:
+
+        [784, 16, 16, 10]
+          +   +   +   +
+          |   |   |   +-----> layer(4) with 10 neurons
+          |   |   |
+          |   |   +---------> layer(3) with 16 neurons
+          |   |
+          |   +-------------> Layer(2) with 16 neurons
+          |
+          +-----------------> Layer(1) with 784 neurons (WARNING: Input layer)
 
     Return: Array with the data information loaded
     """
@@ -138,8 +147,16 @@ class Network:
     def __input_layer(self):
         return self.layers[0]
 
-layers_information = [784, 16, 16, 10]
-n = Network(layers_information)
+    def save_me(self):
+        # NOT USE marshal, use pickle
+        new_file = 'asdf'
+        marshal.dump(self, new_file)
+
+import marshal
+
+n = Network()
+n.create_network('handwritten_digits.json')
 n.start()
 n.print_network()
-n.load_configuration('handwritten_digits.json')
+marshal.dump([n], 'fda')
+# n.load_configuration('')
